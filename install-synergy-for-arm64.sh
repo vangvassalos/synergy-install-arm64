1828471ac30f5b2754c57d96ed775a72adb9d4d2
vassalos <vassalos@iridalabs.gr>
2021-04-26 12:21:47 +0300

    initial commit

---
+++ install-synergy-for-arm64.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+sudo dpkg -i ./libavahi-compat-libdnssd1_0.6.32_rc+dfsg-1ubuntu2_arm64.deb
+sudo dpkg -i ./libcrypto++9v5_5.6.1-9ubuntu0.1_arm64.deb
+sudo dpkg -i ./synergy_1.6.2-0ubuntu2_arm64.deb
